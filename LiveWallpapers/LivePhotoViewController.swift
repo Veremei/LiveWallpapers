e5ff7cf3a6444cec21a302021417ede5986733d7
Daniel <danila.veremei@gmail.com>
2019-03-30 15:14:20 +0300

    PhotoVC done

---
+++ LiveWallpapers/LivePhotoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LivePhotoViewController.swift
+//  LiveWallpapers
+//
+//  Created by apple on 30/03/2019.
+//  Copyright © 2019 DAN. All rights reserved.
+//
+
+import UIKit
+
+class LivePhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
