77139daf2c2e8aba5e2387ee946c571c1cd7db3a
Daniel <33792509+Veremei@users.noreply.github.com>
2020-01-28 14:45:24 +0300

    clean up

--- LiveWallpapers/Application/Network/AlamofireNetworkRequest.swift
+++ LiveWallpapers/Application/Network/AlamofireNetworkRequest.swift
@@ -11,8 +11,6 @@ import Alamofire
 
 class AlamofireNetworkRequest {
     typealias JSON = [String: AnyObject]
-//    static var onProgress: ((Double) -> ())?
-//    static var completed: ((String) -> ())?
     
     static func sendRequest(url: String, completion: @escaping (_ photos: [Photo],_ meta: Meta, _ links: Links)->()) {
         
@@ -44,87 +42,5 @@ class AlamofireNetworkRequest {
             }
         }
     }
-    
-//    static func downloadImage(url: String, completion: @escaping (_ image: UIImage)->()) {
-//
-//        guard let url = URL(string: url) else { return }
-//
-//        request(url).responseData { (responseData) in
-//
-//            switch responseData.result {
-//            case .success(let data):
-//                guard let image = UIImage(data: data) else { return }
-//                completion(image)
-//            case .failure(let error):
-//                print(error)
-//            }
-//        }
-//    }
-    
-//    static func responseData(url: String) {
-//
-//        request(url).responseData { (responseData) in
-//
-//            switch responseData.result {
-//            case .success(let data):
-//                guard let string = String(data: data, encoding: .utf8) else { return }
-//                print(string)
-//            case .failure(let error):
-//                print(error)
-//            }
-//        }
-//    }
-//
-//    static func responseString(url: String) {
-//
-//        request(url).responseString { (responseString) in
-//
-//            switch responseString.result {
-//            case .success(let string):
-//                print(string)
-//            case .failure(let error):
-//                print(error)
-//            }
-//        }
-//    }
-//
-//    static func response(url: String) {
-//
-//        request(url).response { (response) in
-//
-//            guard
-//                let data = response.data,
-//                let string = String(data: data, encoding: .utf8)
-//                else { return }
-//
-//            print(string)
-//        }
-//    }
-    
-    //    static func downloadImageWithProgress(url: String, completion: @escaping (_ image: UIImage) -> ()) {
-    //
-    //        guard let url = URL(string: url) else { return }
-    //
-    //        request(url)
-    //            .validate()
-    //            .downloadProgress { (progress) in
-    //
-    //                print("totalUnitCount: \(progress.totalUnitCount)\n")
-    //                print("completedUnitCount:\(progress.completedUnitCount)\n")
-    //                print("fractionCompleted:\(progress.fractionCompleted)\n")
-    //                print("loclizedDescription:\(progress.localizedDescription!)\n")
-    //                print("---------------------------------------------------------")
-    //
-    //                self.onProgress?(progress.fractionCompleted)
-    //                self.completed?(progress.localizedDescription)
-    //
-    //            }.response { (response) in
-    //
-    //                guard let data = response.data, let image = UIImage(data: data) else { return }
-    //
-    //                DispatchQueue.main.async {
-    //                    completion(image)
-    //                }
-    //        }
-    //    }
+
 }
